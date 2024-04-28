22df6dfe55ebcf8a8ce10567edbe2a7ef80c9ba1
JacksonMath <jackson.souza01@fatec.sp.gov.br>
2024-04-28 20:22:02 -0300

    Revert "Sistema de Escalada Implementado"
    This reverts commit 9c82ffc268afe41ec199804fa6a82ab746e9b6a0.

--- PI_3/Assets/animationTecnico.cs
+++ PI_3/Assets/animationTecnico.cs
@@ -1,31 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KinematicCharacterController;
 using System;
-
+using KinematicCharacterController.Walkthrough.AddingImpulses;
+using KinematicCharacterController.Walkthrough.ChargingState;
+using KinematicCharacterController.Walkthrough.ClimbingLadders;
+using KinematicCharacterController.Walkthrough.WallJumping;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 
 public class animationTecnico : MonoBehaviour
 
 {
-    public LedgeGrabbing grabbing;
+    public KinematicCharacterController.Walkthrough.ClimbingLadders.MyCharacterController MyCharacterController;
+    public KinematicCharacterController.Walkthrough.ClimbingLadders.CharacterState CharacterState;
+    public KinematicCharacterController.Walkthrough.ClimbingLadders.ClimbingState ClimbingState;
+
+    public KinematicCharacterMotor Motor;
     public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (grabbing.holding)
+        if (ClimbingState != null)
         {
             anim.SetBool("Climb", true);
-        }
-        else
-        {
-            anim.SetBool("Climb", false);
         }
     }
 }
