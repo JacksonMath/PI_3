12a3b900531d9cfa0315ece45cb9b1a4f1b48794
JacksonMath <jackson.souza01@fatec.sp.gov.br>
2024-06-08 21:12:18 -0300

    atualização da cutscene

---
+++ PI_3/Assets/Scripts/activeSmoke.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeSmoke : MonoBehaviour
+{
+    public GameObject targetObject; // O objeto a ser ativado/desativado
+    public AudioClip activationSound; // O som a ser reproduzido na ativação
+    private AudioSource audioSource;
+    public float timer = 3f;
+
+    void Start()
+    {
+        if (targetObject == null)
+        {
+            Debug.LogError("Target object is not assigned.");
+            return;
+        }
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = activationSound;
+        InvokeRepeating("ToggleActiveState", 0f, timer); // Chama o método ToggleActiveState a cada 3 segundos
+    }
+
+    void ToggleActiveState()
+    {
+        if (targetObject != null)
+        {
+            targetObject.SetActive(!targetObject.activeSelf);
+
+            if (targetObject.activeSelf && activationSound != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
